cdf8e011276382d869b6a912a8cc9501129e1b4b
YukeChan2002 <yukeshchandran002@gmail.com>
2024-06-05 19:31:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from "./components/Hello";
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings !</h1>
+      <Hello name="Yukesh"/>
+      <Hello name="Chandran "/>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,5 @@
+const Hello = (props) => {
+  return <p>Hi {props.name}, Welcome to our Application!</p>;
+};
+
+export default Hello;
